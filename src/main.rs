92b22144b4744be3e6ecbcc568d29f05c9246437
Abhinav Yadav <abhinavy14@gmail.com>
2023-01-11 11:08:39 +0530

    added a way to control the path were images are saved by the requests, also formated some code

--- src/main.rs
+++ src/main.rs
@@ -52,11 +52,11 @@ async fn upload_image(
     mut files: Multipart,
 ) -> Result<Json<serde_json::Value>, (StatusCode, Json<serde_json::Value>)> {
     // get the name of aws bucket from env variable
-    let bucket = std::env::var("AWS_S3_BUCKET").unwrap_or("my-bucket-name".to_owned());
+    let bucket = std::env::var("AWS_S3_BUCKET").unwrap_or_else(|_| "my-bucket-name".to_owned());
     // if you have a public url for your bucket, place it as ENV variable BUCKET_URL
-    let bucket_url = std::env::var("BUCKET_URL").unwrap_or(bucket.to_owned());
+    let bucket_url = std::env::var("BUCKET_URL").unwrap_or_else(|_| bucket.to_owned());
     // if a sub folder path is set in environment then get that else set a default sub path
-    let sub_path = std::env::var("BUCKET_SUB_PATH").unwrap_or("uploaded_images".to_owned());
+    let sub_path = std::env::var("BUCKET_SUB_PATH").unwrap_or_else(|_| "uploaded_images".to_owned());
     // we are going to store the respose in HashMap as filename: url => key: value
     let mut res = HashMap::new();
     while let Some(file) = files.next_field().await.unwrap() {
@@ -67,11 +67,14 @@ async fn upload_image(
         let name = file.file_name().unwrap().to_string();
         // file data
         let data = file.bytes().await.unwrap();
+        // the request can control where to save the image on AWS S3
+        let request_path = if category.trim().is_empty() {"".to_owned()} else {format!("{}/", &category.trim_end_matches('/'))};
         // the path of file to store on aws s3 with file name and extention
         // timestamp_category_filename => 14-12-2022_01:01:01_customer_somecustomer.jpg
         let key = format!(
-            "{}/images/{}_{}_{}",
+            "{}/images/{}{}_{}_{}",
             sub_path,
+            &request_path,
             chrono::Utc::now().format("%d-%m-%Y_%H:%M:%S"),
             &category,
             &name
